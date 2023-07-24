b11ff243f180a8ef6b389501b10e927cbe3b06b5
jepson <jiangpeng@itcast.cn>
2023-07-25 03:59:45 +0800

    完善表单结构

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -1,7 +1,20 @@
 <script setup>
 import { ref } from 'vue'
+import ChannelSelect from './ChannelSelect.vue'
 // 控制抽屉显示隐藏
 const visibleDrawer = ref(false)
+
+// 默认数据
+const defaultForm = {
+  title: '', // 标题
+  cate_id: '', // 分类id
+  cover_img: '', // 封面图片 file 对象
+  content: '', // string 内容
+  state: '' // 状态
+}
+
+// 准备数据
+const formModel = ref({ ...defaultForm })
 
 // 组件对外暴露一个方法 open，基于open传来的参数，区分添加还是编辑
 // open({})  => 表单无需渲染，说明是添加
@@ -9,16 +22,47 @@ const visibleDrawer = ref(false)
 // open调用后，可以打开抽屉
 const open = (row) => {
   visibleDrawer.value = true // 显示抽屉
-  console.log(row)
+
+  if (row.id) {
+    // 需要基于 row.id 发送请求，获取编辑对应的详情数据，进行回显
+    console.log('编辑回显')
+  } else {
+    formModel.value = { ...defaultForm } // 基于默认的数据，重置form数据
+    console.log('添加')
+  }
 }
 
 defineExpose({
   open
 })
 </script>
+
 <template>
-  <!-- 抽屉 -->
-  <el-drawer v-model="visibleDrawer" title="大标题" direction="rtl" size="50%">
-    <span>Hi there!</span>
+  <el-drawer
+    v-model="visibleDrawer"
+    :title="formModel.id ? '编辑文章' : '添加文章'"
+    direction="rtl"
+    size="50%"
+  >
+    <!-- 发表文章表单 -->
+    <el-form :model="formModel" ref="formRef" label-width="100px">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="formModel.title" placeholder="请输入标题"></el-input>
+      </el-form-item>
+      <el-form-item label="文章分类" prop="cate_id">
+        <channel-select
+          v-model="formModel.cate_id"
+          width="100%"
+        ></channel-select>
+      </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img"> 文件上传 </el-form-item>
+      <el-form-item label="文章内容" prop="content">
+        <div class="editor">富文本编辑器</div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button type="info">草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-drawer>
 </template>
